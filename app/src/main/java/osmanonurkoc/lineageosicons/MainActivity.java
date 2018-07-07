fc2fc254fc61e0b5714a4a2111965c338e7f6d1e
osmanonurkoc <osmanonurkoc@gmail.com>
2018-07-07 16:42:51 +0300

    “v1”

---
+++ app/src/main/java/osmanonurkoc/lineageosicons/MainActivity.java
@@ -0,0 +1,13 @@
+package osmanonurkoc.lineageosicons;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
